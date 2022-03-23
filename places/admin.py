55bfa70b0f222975a969665909dba2033feca374
Anastasia <asya_apto@mail.ru>
2022-03-23 18:22:28 +0300

    Add Image model

--- places/admin.py
+++ places/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 
-from places.models import Place
+from places.models import Place, Image
 
 # Register your models here.
 admin.site.register(Place)
+
+
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'place')
--- places/models.py
+++ places/models.py
@@ -10,3 +10,14 @@ class Place(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Image(models.Model):
+    image = models.ImageField(upload_to='./media/')
+    place = models.ForeignKey('Place',
+                             related_name='image',
+                             on_delete=models.CASCADE,
+                             null=True)
+
+    def __str__(self):
+        return f'Картинка id {self.id}'
