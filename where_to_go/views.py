e6217006216cb26f006ccbadfdaea0637de9dc78
Anastasia <asya_apto@mail.ru>
2022-03-24 17:42:08 +0300

    Move data from template to view

--- where_to_go/views.py
+++ where_to_go/views.py
@@ -1,6 +1,33 @@
+import os
+
+from functools import reduce
 from django.shortcuts import render
+
+from places.models import Place
 
 
 def render_main_page(request):
-    print('Кто-то зашёл на главную!')
-    return render(request, 'index.html')
+    places = Place.objects.all()
+    places_details = {
+        "type": "FeatureCollection",
+        "features": []
+    }
+    for place in places:
+        feature = {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [place.coordinates_lng, place.coordinates_lat]
+            },
+            "properties": {
+                "title": place.title,
+                "placeId": place.pk,
+                "detailsUrl": reduce(
+                    os.path.join,
+                    ['static', 'places', place.feature_filename])
+                }
+            }
+        places_details['features'].append(feature)
+
+    context = {"places_details": places_details}
+    return render(request, 'index.html', context)
