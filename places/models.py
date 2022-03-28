a2eead4f5ee3caa1a7d878d2074236dfdbc0b956
Anastasia <asya_apto@mail.ru>
2022-03-28 17:01:55 +0300

    Add verbose_names

--- places/models.py
+++ places/models.py
@@ -2,23 +2,34 @@ from django.db import models
 
 
 class Place(models.Model):
-    title = models.CharField(max_length=100)
-    description_short = models.TextField()
-    description_long = models.TextField()
-    coordinates_lng = models.DecimalField(max_digits=16, decimal_places=14)
-    coordinates_lat = models.DecimalField(max_digits=16, decimal_places=14)
+    title = models.CharField('Заголовок', max_length=100)
+    description_short = models.TextField('Краткое описание')
+    description_long = models.TextField('Полное описание')
+    coordinates_lng = models.DecimalField('Широта',
+                                          max_digits=8, decimal_places=6)
+    coordinates_lat = models.DecimalField('Долгота',
+                                          max_digits=8, decimal_places=6)
+
+    class Meta:
+        verbose_name = 'Место'
+        verbose_name_plural = 'Места'
 
     def __str__(self):
         return self.title
 
 
 class Image(models.Model):
-    image = models.ImageField(upload_to='')
+    image = models.ImageField('Картинка', upload_to='')
     place = models.ForeignKey('Place',
-                             related_name='image',
-                             on_delete=models.CASCADE,
-                             null=True)
-    position = models.SmallIntegerField(null=True, blank=True)
+                              verbose_name='Место',
+                              related_name='image',
+                              on_delete=models.CASCADE,
+                              null=True)
+    position = models.SmallIntegerField('Позиция', null=True, blank=True)
+
+    class Meta:
+        verbose_name = 'Картинка'
+        verbose_name_plural = 'Картинки'
 
     def __str__(self):
         return f'Картинка id {self.id}'
